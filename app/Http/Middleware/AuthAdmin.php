7a6278b57321564a1103fe33aecb3aa285f7d7fb
Stanislav Mikhailov <samikhajlov@gmail.com>
2016-07-14 08:57:40 +1000

    add auth, admin panel and admin auth

---
+++ app/Http/Middleware/AuthAdmin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class AuthAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {   
+       
+        if(Auth::check()) {
+            if(Auth::user()->admin==1) {
+                return $next($request);
+            }
+        }
+        else {
+            return redirect('/login');
+        }
+        return redirect('/');
+    }
+}
